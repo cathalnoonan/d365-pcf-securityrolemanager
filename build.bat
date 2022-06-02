ffc9048ccca3034223bb99274ef3d0fac9a2600d
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2022-06-02 22:51:47 +0100

    Build related updates

--- build.bat
+++ build.bat
@@ -1,10 +1,3 @@
 @echo off
 
-@REM # Install dependencies for pcf project
-cd ./control
-call npm install --no-audit
-call npm audit --prod
-cd ..
-
-@REM # Build the solution
 call dotnet build ./solution -c Release
--- build.sh
+++ build.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-# Use node version manager to install node version consistently
-if (command -v nvm) ; then
-  nvm install && nvm use 
-fi
-
-# Install dependencies for pcf project
-npm --prefix ./control install --no-audit
-npm --prefix ./control audit --prod
-
-# Build the solution
 dotnet build ./solution -c Release
