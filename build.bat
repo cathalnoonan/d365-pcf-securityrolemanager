34f129538cfff31ab2de6390aade79b4ea977cc6
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2022-06-07 21:53:21 +0100

    Build updates

--- build.bat
+++ build.bat
@@ -1,3 +1,9 @@
 @echo off
 
+@REM On windows, we can't rely on npm's "--prefix" option
+cd ./control
+call npm install --no-audit --ignore-scripts --no-fund
+call npm audit --prod
+cd ../
+
 call dotnet build ./solution -c Release
--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+npm install --prefix ./control --no-audit --ignore-scripts --no-fund
+npm audit --prefix ./control --prod
+
 dotnet build ./solution -c Release
