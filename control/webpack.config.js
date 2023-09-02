c76cb93bf84fe9b2aa97dbe6d1cc3e3cc51e8f8c
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2023-09-02 18:56:36 +0100

    Update repo template files

--- control/webpack.config.js
+++ control/webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   // Generate a source map to make debugging easier.
   devtool: 'source-map',
 
-  // Support older browsers.
-  target: ['web', 'es5'],
+  // Use "ES5" to support older browsers.
+  // IE11 is end of support, so this shouldn't be necessary anymore.
+  target: ['web', 'es2015'],
 }
