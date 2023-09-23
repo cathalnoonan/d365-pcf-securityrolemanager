88274137c3be272028611a6a11f7b146778aedfa
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2023-09-23 17:55:41 +0100

    Include source map in the browser

--- control/webpack.config.js
+++ control/webpack.config.js
@@ -1,5 +1,7 @@
 // @ts-check
 
+const packageJson = require('./package.json')
+
 /**
  * @type { import('webpack').Configuration }
  */
@@ -10,4 +12,9 @@ module.exports = {
   // Use "ES5" to support older browsers.
   // IE11 is end of support, so this shouldn't be necessary anymore.
   target: ['web', 'es2017'],
+
+  output: {
+    // Set the uniqueName so the source map is easier to find in the browser.
+    uniqueName: packageJson.name,
+  },
 }
